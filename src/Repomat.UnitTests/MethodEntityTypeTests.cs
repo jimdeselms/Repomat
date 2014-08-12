a02feb72d0c2f458421e5ae82b6160db505a1ab9
Jim DeSelms <jim.deselms@gmail.com>
2014-08-12 00:57:06 -0400

    Committing a bug to remind myself to fix the fact that GetCount and Exists fail if there are no

--- src/Repomat.UnitTests/MethodEntityTypeTests.cs
+++ src/Repomat.UnitTests/MethodEntityTypeTests.cs
@@ -34,6 +34,45 @@ namespace Repomat.UnitTests
             repo.DropTable();
         }
 
+        [Test]
+        public void InterfaceThatHasTwoEntityTypes()
+        {
+            // If an interface only has standard void functions, then you explicitly have to specify the type.
+            var dlBuilder = DataLayerBuilder.DefineSqlDatabase(Connections.NewSqlConnection());
+            var repoBuilder = dlBuilder.SetupRepo<ITwoEntities>();
+            repoBuilder.SetupMethod("CreateTablePerson").SetEntityType(typeof(Person));
+            repoBuilder.SetupMethod("DropTablePerson").SetEntityType(typeof(Person));
+            repoBuilder.SetupMethod("TableExistsPerson").SetEntityType(typeof(Person));
+            repoBuilder.SetupMethod("GetCountFromPerson").SetEntityType(typeof(Person));
+            repoBuilder.SetupMethod("CreateTableFoo").SetEntityType(typeof(Foo));
+            repoBuilder.SetupMethod("DropTableFoo").SetEntityType(typeof(Foo));
+            repoBuilder.SetupMethod("TableExistsFoo").SetEntityType(typeof(Foo));
+            repoBuilder.SetupMethod("GetCountFromFoo").SetEntityType(typeof(Foo));
+
+            var repo = dlBuilder.CreateRepo<ITwoEntities>();
+
+            if (repo.TableExistsPerson())
+            {
+                repo.DropTablePerson();
+            }
+            repo.CreateTablePerson();
+
+            if (repo.TableExistsFoo())
+            {
+                repo.DropTableFoo();
+            }
+            repo.CreateTableFoo();
+
+            Foo f = new Foo { Bar = "blah" };
+            repo.Insert(f);
+
+            Assert.AreEqual(0, repo.GetCountFromPerson());
+            Assert.AreEqual(1, repo.GetCountFromFoo());
+
+            repo.DropTableFoo();
+            repo.DropTablePerson();
+        }
+
         public interface IOnlyVoidMethods
         {
             void CreateTable();
@@ -41,6 +80,28 @@ namespace Repomat.UnitTests
             bool TableExists();
 
             int GetCountFromPerson();
+        }
+
+        public interface ITwoEntities
+        {
+            void CreateTablePerson();
+            void DropTablePerson();
+            bool TableExistsPerson();
+
+            void CreateTableFoo();
+            void DropTableFoo();
+            bool TableExistsFoo();
+
+            void Insert(Person p);
+            void Insert(Foo f);
+
+            int GetCountFromPerson();
+            int GetCountFromFoo();
+        }
+
+        public class Foo
+        {
+            public string Bar { get; set; }
         }
     }
 }
