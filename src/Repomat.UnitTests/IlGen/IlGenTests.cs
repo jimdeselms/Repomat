358accf5f5439a7680aaffdc93c0dc901b0a9aa3
Jim DeSelms <jim.deselms@gmail.com>
2014-08-20 00:15:34 -0400

    Starting to switch repo generation to IL instead of compiled C#. This will be faster and prevent issues related to referencing assemblies that we don't know how to find.

---
+++ src/Repomat.UnitTests/IlGen/IlGenTests.cs
@@ -0,0 +1,77 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Reflection.Emit;
+using Repomat.IlGen;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Repomat.UnitTests.IlGen
+{
+    [TestFixture]
+    public class IlGenTests
+    {
+        [Test]
+        public void BuildClass()
+        {
+            var asmBuilder = AppDomain.CurrentDomain.DefineDynamicAssembly(new AssemblyName("MyAssembly"), AssemblyBuilderAccess.RunAndSave);
+            var modBuilder = asmBuilder.DefineDynamicModule("MyModule");
+            var typeBuilder = modBuilder.DefineType("MyClass");
+            var myField = typeBuilder.DefineField("_this", typeof(int), FieldAttributes.Public);
+            
+            var ctorBuilder = typeBuilder.DefineConstructor(MethodAttributes.Public, CallingConventions.Standard | CallingConventions.HasThis, new Type[0]);
+                        
+            var ilGen = ctorBuilder.GetILGenerator();
+            
+//            ilGen.Emit(OpCodes.Ldarg_0);
+//            var objectCtor = typeof(object).GetConstructor(new Type[0]);
+//            ilGen.Emit(OpCodes.Call, objectCtor);
+
+            ilGen.Emit(OpCodes.Ldarg_0);
+            ilGen.Emit(OpCodes.Ldc_I4_S, (byte)25);
+//            ilGen.Emit(OpCodes.Pop);
+            ilGen.Emit(OpCodes.Stfld, myField);
+            ilGen.Emit(OpCodes.Ret);
+
+            var type = typeBuilder.CreateType();
+            var obj = type.GetConstructor(new Type[0]).Invoke(new object[0]);
+
+            var fieldInfo = obj.GetType().GetField("_this");
+            Assert.AreEqual(25, fieldInfo.GetValue(obj));
+        }
+
+        [Test]
+        public void BuildConnectionBasedRepo()
+        {
+            RepoIlBuilder b = new RepoIlBuilder(typeof(INothing), RepoConnectionType.SingleConnection);
+            Type t = b.CreateType();
+
+            var ctor = t.GetConstructor(new Type[] { typeof(IDbConnection) });
+
+            var conn = new SqlConnection();
+            var repo = ctor.Invoke(new object[] { conn });
+
+            Assert.AreSame(conn, repo.GetType().GetField("_connection", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(repo));
+        }
+
+        [Test]
+        public void BuildConnectionFactoryBasedRepo()
+        {
+            RepoIlBuilder b = new RepoIlBuilder(typeof(INothing), RepoConnectionType.ConnectionFactory);
+            Type t = b.CreateType();
+
+            var ctor = t.GetConstructor(new Type[] { typeof(Func<IDbConnection>) });
+
+            Func<IDbConnection> connFactory = () => new SqlConnection();
+            var repo = ctor.Invoke(new object[] { connFactory });
+
+            Assert.AreSame(connFactory, repo.GetType().GetField("_connectionFactory", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(repo));
+        }
+
+        public interface INothing { }
+    }
+}
---
+++ src/Repomat/IlGen/RepoIlBuilder.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repomat.IlGen
+{
+    internal class RepoIlBuilder
+    {
+        private static int _nextRepoSuffix = 1;
+
+        private static readonly ModuleBuilder _moduleBuilder;
+
+        static RepoIlBuilder()
+        {
+            var asmBuilder = AppDomain.CurrentDomain.DefineDynamicAssembly(new System.Reflection.AssemblyName("RepomatDynamicRepos"), AssemblyBuilderAccess.Run);
+            _moduleBuilder = asmBuilder.DefineDynamicModule("Repos");
+        }
+
+        private readonly TypeBuilder _typeBuilder;
+
+        private FieldBuilder _connectionField;
+
+        public RepoIlBuilder(Type interfaceType, RepoConnectionType repoConnectionType)
+        {
+            _typeBuilder = _moduleBuilder.DefineType(interfaceType.FullName + "_" + _nextRepoSuffix++);
+            _typeBuilder.AddInterfaceImplementation(interfaceType);
+
+            switch (repoConnectionType)
+            {
+                case RepoConnectionType.SingleConnection:
+                    DefineSingleConnectionCtor();
+                    break;
+                case RepoConnectionType.ConnectionFactory:
+                    DefineConnectionFactoryCtor();
+                    break;
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+
+        public Type CreateType()
+        {
+            return _typeBuilder.CreateType();
+        }
+
+        private void DefineSingleConnectionCtor()
+        {
+            DefineConstructor("_connection", typeof(IDbConnection));
+        }
+
+        private void DefineConnectionFactoryCtor()
+        {
+            DefineConstructor("_connectionFactory", typeof(Func<IDbConnection>));
+        }
+
+        private void DefineConstructor(string fieldName, Type fieldType)
+        {
+            _connectionField = _typeBuilder.DefineField(fieldName, fieldType, FieldAttributes.Private);
+
+            var ctor = _typeBuilder.DefineConstructor(MethodAttributes.Public, CallingConventions.Standard | CallingConventions.HasThis, new Type[] { fieldType });
+            var ilBuilder = ctor.GetILGenerator();
+            ilBuilder.Emit(OpCodes.Ldarg_0);
+            ilBuilder.Emit(OpCodes.Ldarg_1);
+            ilBuilder.Emit(OpCodes.Stfld, _connectionField);
+            ilBuilder.Emit(OpCodes.Ret);
+        }
+    }
+
+    internal enum RepoConnectionType
+    {
+        SingleConnection,
+        ConnectionFactory,
+    }
+}
